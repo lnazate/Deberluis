dad6cea12d985cc85f35573b1213786263130540
Luis <lnazate3616@utm.edu.ec>
2023-10-29 21:12:33 -0500

    Commit 3 nivel de dificultad y operadores

--- src/main/java/com/mycompany/deberluis/GeneradorYRastreador.java
+++ src/main/java/com/mycompany/deberluis/GeneradorYRastreador.java
@@ -11,4 +11,17 @@ public class GeneradorYRastreador {
         this.random = new Random();
     }
 
+    public int[] generarOperandos() {
+        int limite = (int) Math.pow(10, nivelDificultad);
+        return new int[]{random.nextInt(limite), random.nextInt(limite)};
+    }
+
+    public String generarPregunta(int operando1, int operando2) {
+        return "¿Cuánto es " + operando1 + " + " + operando2 + "? ";
+    }
+
+    public boolean esRespuestaCorrecta(int operando1, int operando2, int respuestaUsuario) {
+        return operando1 + operando2 == respuestaUsuario;
+    }
+
 }
